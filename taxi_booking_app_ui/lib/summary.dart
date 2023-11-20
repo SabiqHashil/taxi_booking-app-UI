f3f63a9fc20ee1499f2ba1c66296859e279ca4f3
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-11-20 23:21:28 +0530

    update

--- taxi_booking_app_ui/lib/summary.dart
+++ taxi_booking_app_ui/lib/summary.dart
@@ -5,6 +5,51 @@ class Summary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Summary"),
+        backgroundColor: Colors.amber[600],
+        elevation: 0,
+        toolbarHeight: 100,
+        actions: [
+          Icon(Icons.settings),
+          SizedBox(
+            width: 20,
+          ),
+        ],
+      ),
+      body: content(),
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          width: double.infinity,
+          height: 200,
+          decoration: BoxDecoration(
+            color: Colors.amber[200],
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/taxi.png',
+                width: 160,
+                height: 160,
+              ),
+              Text(
+                "\$20",
+                style: TextStyle(fontSize: 50),
+              )
+            ],
+          ),
+        )
+      ],
+    );
   }
 }
