1e30411e82bf8abf9def181b9f59be57bcb6496f
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-11-20 23:27:05 +0530

    updated

--- taxi_booking_app_ui/lib/summary.dart
+++ taxi_booking_app_ui/lib/summary.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_booking_app_ui/booking.dart';
 
 class Summary extends StatelessWidget {
   const Summary({super.key});
@@ -23,33 +24,58 @@ class Summary extends StatelessWidget {
   }
 
   Widget content() {
-    return Column(
-      children: [
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          width: double.infinity,
-          height: 200,
-          decoration: BoxDecoration(
-            color: Colors.amber[200],
-            borderRadius: BorderRadius.circular(20),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 20,
           ),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/taxi.png',
-                width: 160,
-                height: 160,
+          Container(
+            width: double.infinity,
+            height: 200,
+            decoration: BoxDecoration(
+              color: Colors.amber[200],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/taxi.png',
+                  width: 160,
+                  height: 160,
+                ),
+                Text(
+                  "\$20",
+                  style: TextStyle(fontSize: 50),
+                )
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          locationInputField("XXX,XXX", false),
+          Text("|\n|"),
+          locationInputField("XXX,XXX", false),
+          SizedBox(
+            height: 60,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Rate your last ride : ",
+                style: TextStyle(fontSize: 20),
               ),
-              Text(
-                "\$20",
-                style: TextStyle(fontSize: 50),
-              )
-            ],
+            ),
           ),
-        )
-      ],
+          SizedBox(
+            height: 10,
+          )
+        ],
+      ),
     );
   }
 }
