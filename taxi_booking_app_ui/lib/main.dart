dd316ad62a7880a986b59d0a83f4f7a5692ab3f0
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-11-20 23:13:56 +0530

    summary added

--- taxi_booking_app_ui/lib/main.dart
+++ taxi_booking_app_ui/lib/main.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:taxi_booking_app_ui/booking.dart';
 import 'package:taxi_booking_app_ui/home.dart';
 import 'package:taxi_booking_app_ui/paymentMethod.dart';
+import 'package:taxi_booking_app_ui/summary.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      // initialRoute: "/paymentMethod",
+      initialRoute: "/summary",
       routes: {
         '/': ((context) => const Home()),
         '/booking': ((context) => const Booking()),
-        '/paymentMethod': ((context) => const PaymentMethod())
+        '/paymentMethod': ((context) => const PaymentMethod()),
+        '/summary': ((context) => const Summary()),
       },
     ));
